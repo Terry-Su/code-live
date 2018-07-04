594c5d16b9568bc408cf82d28bfb58445fc197b3
TerrySu <2684759855@qq.com>
2018-07-05 00:03:31 +0800

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,64 +1,82 @@
-const viewerBox = document.getElementById( 'viewerBox' )
+const viewerBox = document.getElementById('viewerBox')
 const iframeDomString = '<iframe id="iframe" src="./iframe.html" frameborder="0"></iframe>'
 
 /**
  * // Textarea
  */
-const html = document.getElementById( 'html' )
-const css = document.getElementById( 'css' )
-const javascript = document.getElementById( 'javascript' )
+const html = document.getElementById('html')
+const css = document.getElementById('css')
+const javascript = document.getElementById('javascript')
 
 /**
  * // Tab buttons
  */
-const htmlBtn = document.getElementById( 'htmlBtn' )
-const cssBtn = document.getElementById( 'cssBtn' )
-const javascriptBtn = document.getElementById( 'javascriptBtn' )
-const resultBtn = document.getElementById( 'resultBtn' )
+const htmlBtn = document.getElementById('htmlBtn')
+const cssBtn = document.getElementById('cssBtn')
+const javascriptBtn = document.getElementById('javascriptBtn')
+const resultBtn = document.getElementById('resultBtn')
 
+init()
 bindTabButtonsEvents()
 bindTextareasEvents()
+hideAll()
+showElement(html)
+
 refreshIframe()
 
-hideAll()
-showElement( html )
+function init() {
 
+  window.addEventListener("message", receiveMessage);
+
+  function receiveMessage(event) {
+    const { tsHtml, tsCss, tsJavascript } = event.data || {}
+    console.log( event.data )
+    if ( tsHtml !== undefined ) {
+      html.innerHTML = tsHtml
+    }
+    if ( tsCss !== undefined ) {
+      css.innerHTML = tsCss
+    }
+    if ( tsJavascript !== undefined ) {
+      html.innerHTML = tsJavascript
+    }
+    refreshIframe()
+  }
+}
 
 function bindTabButtonsEvents() {
   htmlBtn.onclick = onHtmlBtnClick
-  cssBtn.onclick = onCSSBtnClick
+  cssBtn.onclick = onCssBtnClick
   javascriptBtn.onclick = onJavascriptBtnClick
   resultBtn.onclick = onResultBtnClick
 
   function onHtmlBtnClick() {
     hideAll()
-    toggleDisplayElement( html )
+    toggleDisplayElement(html)
   }
 
-  function onCSSBtnClick() {
+  function onCssBtnClick() {
     hideAll()
-    toggleDisplayElement( css )
+    toggleDisplayElement(css)
   }
 
   function onJavascriptBtnClick() {
     hideAll()
-    toggleDisplayElement( javascript )
+    toggleDisplayElement(javascript)
   }
 
   function onResultBtnClick() {
     hideAll()
   }
-
-  
 }
 
 function bindTextareasEvents() {
-  html.addEventListener( 'input', onChangeListener)
-  html.addEventListener( 'onpropertychange', onChangeListener)
-  css.addEventListener( 'input', onChangeListener)
-  css.addEventListener( 'onpropertychange', onChangeListener)
-  javascript.addEventListener( 'input', onChangeListener)
-  javascript.addEventListener( 'onpropertychange', onChangeListener)
+  html.addEventListener('input', onChangeListener)
+  html.addEventListener('onpropertychange', onChangeListener)
+  css.addEventListener('input', onChangeListener)
+  css.addEventListener('onpropertychange', onChangeListener)
+  javascript.addEventListener('input', onChangeListener)
+  javascript.addEventListener('onpropertychange', onChangeListener)
 
   function onChangeListener() {
     refreshIframe()
@@ -73,17 +91,18 @@ function refreshIframe() {
 
 function loadIframe() {
 
-  viewerBox.innerHTML = iframeDomString
 
   window.removeEventListener('myEvent', communicationLister)
   window.addEventListener('myEvent', communicationLister)
 
-  function communicationLister(e) {
-    const { updateHtmlFn, updateCSSFn, updateJavascriptFn } = e.detail
+  viewerBox.innerHTML = iframeDomString
 
-      updateHtmlFn( html.value )  
-      updateCSSFn( css.value )  
-      updateJavascriptFn( javascript.value )  
+  function communicationLister(e) {
+    const {updateHtmlFn, updateCssFn, updateJavascriptFn} = e.detail
+
+    updateHtmlFn(html.value)
+    updateCssFn(css.value)
+    updateJavascriptFn(javascript.value)
   }
 }
 
@@ -96,17 +115,17 @@ function unloadIframe() {
 /**
  * // Style
  */
-function showElement( element ) {
+function showElement(element) {
   element.style.display = 'block'
 }
-function hideElement( element ) {
+function hideElement(element) {
   element.style.display = 'none'
 }
-function toggleDisplayElement( element ) {
-  if ( element.style.display === 'none' ) {
-    return showElement( element ) 
-  } 
-  hideElement( element )
+function toggleDisplayElement(element) {
+  if (element.style.display === 'none') {
+    return showElement(element)
+  }
+  hideElement(element)
 }
 
 function hideAll() {
